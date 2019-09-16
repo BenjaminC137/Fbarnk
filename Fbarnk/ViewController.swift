56ddf7fbfce8c3d8ff1f5fada08245930dd14205
Benjamin Salasek <benjamin.salasek@icloud.com>
2019-09-15 22:52:21 -0500

    fart and rotate aloe

--- Fbarnk/ViewController.swift
+++ Fbarnk/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var changingText: UITextField!
+    @IBOutlet weak var aloeImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func buttonChangy(_ sender: UIButton) {
+        changingText.text = "fart"
+    }
+    
+    @IBAction func rotateImage(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.5, animations: {
+            self.aloeImage.transform = CGAffineTransform(rotationAngle: (180.0 * .pi) / 180.0)
+        })
+    }
 }
 
